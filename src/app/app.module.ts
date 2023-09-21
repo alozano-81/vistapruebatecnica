b7724909c445e382630a4e279fa9dadafeebd1c3
Andres Lozano <yeisonandreslozano@gmail.com>
2023-09-20 23:24:18 -0500

    Se agregan archivos de prueba, el componente principal donde se encuentra el login esta en construccion

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
